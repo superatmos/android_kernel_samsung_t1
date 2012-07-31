027a07eb16d9957de647264aa411e5ad5207a424
fuss132 <homepagechecker@gmx.de>
2012-07-31 10:39:26 +0200

    initial stockkernel with source from superatmos

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Tue Jul 31 10:33:29 CEST 2012"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "fuss132-LIFEBOOK-AH531-GFO"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
